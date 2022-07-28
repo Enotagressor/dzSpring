584d0d3749cf6e38c44a72829be3b87f2ee4fc87
Sergey <enotagressor@gmail.com>
2022-07-28 17:02:45 +0700

    1

---
+++ src/main/java/pro/sky/dzSpring/DzSpringApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.dzSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DzSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DzSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/dzSpring/DzSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.dzSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DzSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
